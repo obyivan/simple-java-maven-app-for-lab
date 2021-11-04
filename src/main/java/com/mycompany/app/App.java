565b0fac4b11b58ba95319ff63d6b289e5d83a1c
Ivan Pokaliuk <Ivan_Pokaliuk@epam.com>
2021-11-04 20:01:31 +0300

    add comment to end and empty lines

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,12 +6,16 @@ package com.mycompany.app;
 public class App
 {
 
+
+
     private final String message = "Hello World!";
 
     public App() {}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+
+
     }
 
     private final String getMessage() {
@@ -19,3 +23,4 @@ public class App
     }
 
 }
+//last comment
