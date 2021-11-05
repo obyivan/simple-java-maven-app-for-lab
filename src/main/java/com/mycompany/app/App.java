b23bd41b5dcc5cd9d2470e877860ac7d80dee49e
obyivan <93244047+obyivan@users.noreply.github.com>
2021-11-05 16:46:03 +0300

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -16,17 +16,12 @@ public class App
     private final String getMessage() {
         return message;
     }
-
+/*
     public void infiniteLoopUsingDoWhile() {
     while (true) {
          System.out.println("Running while loop");
     	} 
     }    
+*/
 }
-
-    class HelloWorld {
-	public static void main(String[] args){
-			System.out.println("Hello world"); // This is a trailing comment that can be very very long
-		}
-	}
 
